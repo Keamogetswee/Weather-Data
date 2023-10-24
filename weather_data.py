76eda68497975f0507bd73299c5a7b57f639777e
Kea <moseki@Kea-Moseki>
2023-10-24 16:29:52 +0200

    documentation and clean up

--- weather_data.py
+++ weather_data.py
@@ -5,6 +5,18 @@ from process_csv import process_line
 from read_csv import read_weather_data
 from export_csv import export_to_csv_file
 
+# Initialize the database.
+# Read weather data from the 'weather-data.txt' file.
+# Process each line of data:
+#         - If the line contains less than 5 elements, it is skipped.
+#         - Extract the date, city, temperature, humidity, and condition from the line.
+#         - If temperature is not numeric, set it to None.
+#         - Convert the temperature to Fahrenheit.
+#         - Categorize the weather based on temperature.
+#         - Process the line and insert the data into the database.
+# Retrieve specific data from the database (e.g., data for the city 'Johannesburg').
+# Export the retrieved data to a CSV file 'filtered_weather_data.csv'.
+# This function serves as the entry point for running the Weather Data.
 def main():
     initialize_db()
     retrieved_data = read_weather_data('weather-data.txt')
@@ -25,8 +37,6 @@ def main():
         process_line(date, city, temperature_fahrenheit, humidity, condition, weather_category)
         filtered_data = retrive_specific_data("Johannesburg")
         export_to_csv_file(filtered_data)
-        
-        
 
 # Call the main function
 main()
